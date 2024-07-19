f4a2d082054349e0e84a7d3b899df024c57a012f
SaiUttej2k03 <103807636+SaiUttej2k03@users.noreply.github.com>
2024-07-19 09:47:44 +0530

    Create Middle of the list.cpp

---
+++ Middle of the list.cpp
@@ -0,0 +1,34 @@
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slp=head, *fastp=head;
+        while(fastp && fastp->next)
+        {
+            slp=slp->next,fastp=fastp->next->next;
+
+        }
+        return slp;
+    }
+};
